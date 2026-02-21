3817e5c684f60deb8d3bc750ad890e493750c296
Douglas Rintoul <dougirintoul@gmail.com>
2026-02-21 19:36:35 +1100

    task: Add two queries to pulse_reporting.sql

--- sql/pulse_reporting.sql
+++ sql/pulse_reporting.sql
@@ -15,6 +15,27 @@ CREATE TABLE PulseEntry (
         FOREIGN KEY (CategoryId) REFERENCES PulseCategory(Id)
 );
 
--- TODO: Query that matches /api/pulse/summary
+-- /api/pulse/summary Query
+SELECT
+    COUNT(*) AS TotalCount,
+    -- The inner cast ensures correct math, the outer cast rounds to 2 decimal places
+    CAST(AVG(CAST(Score AS DECIMAL(3,2))) AS DECIMAL(3,2)) AS AverageScore,
+    -- Using this approach with Case only requires a single scan of the table (I believe),
+    -- but wouldn't be ideal if the score range was likely to change.
+    -- Also, to be honest I haven't worked that much with SQL pivots, so this is a 
+    -- more comfortable approach for me.
+    COUNT(CASE WHEN Score = 1 THEN 1 END) AS Score1,
+    COUNT(CASE WHEN Score = 2 THEN 1 END) AS Score2,
+    COUNT(CASE WHEN Score = 3 THEN 1 END) AS Score3,
+    COUNT(CASE WHEN Score = 4 THEN 1 END) AS Score4,
+    COUNT(CASE WHEN Score = 5 THEN 1 END) AS Score5
+FROM PulseEntry;
 
--- TODO: Query to return the PulseEntry total counts by PulseCategory
+-- Query to return the PulseEntry total counts by PulseCategory
+SELECT
+    c.Id,
+    c.Name,
+    COUNT (e.Id) AS TotalCount
+FROM PulseCategory pc
+LEFT JOIN PulseEntry e ON e.CategoryId = c.Id
+GROUP BY c.Id, c.Name;
